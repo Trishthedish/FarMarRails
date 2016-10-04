f0dd8f9ed69c84deea97636906ea8adc3b618d27
Trish Griego <pmgriego@gmail.com>
2016-10-04 16:11:51 -0700

    Created vendor model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004230735) do
+ActiveRecord::Schema.define(version: 20161004231112) do
 
   create_table "markets", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20161004230735) do
     t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "num_employees"
+    t.integer  "market_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
