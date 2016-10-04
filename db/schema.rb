b7b60b60ffea264322f50e1733ba24b91b80dcd0
Trish Griego <pmgriego@gmail.com>
2016-10-04 16:16:04 -0700

    Created model for product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004231431) do
+ActiveRecord::Schema.define(version: 20161004231545) do
 
   create_table "markets", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20161004231431) do
     t.string   "county"
     t.string   "state"
     t.string   "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "vendor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
