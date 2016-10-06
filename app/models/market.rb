aee2911c347f62ae596a5eaa988632fd1f10551e
Trish Griego <pmgriego@gmail.com>
2016-10-06 11:36:35 -0700

    Created validations for creating a new market so I dont save commas. Also move the position of the edit market button.

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,15 @@
 class Market < ActiveRecord::Base
 	has_many :vendors
+
+	# name cannot be blank
+	validates :name, presence: true
+	# address cannot be blank
+	validates :address, presence: true
+	# state cannot be blank
+	validates :state, presence: true
+	# zip cannot be blank
+	validates :zip, presence: true
+#other potential validations:
+	# make sure zip code is strictly numbers
+
 end
