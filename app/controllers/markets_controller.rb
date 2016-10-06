e85106b640f9957c104b8fd12cce660d2f03a76c
Trish Griego <pmgriego@gmail.com>
2016-10-05 19:00:01 -0700

    Able to create new market \!

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -8,6 +8,16 @@ class MarketsController < ApplicationController
   end
 
   def create
+    @params = params
+    @mymarket= Market.new
+    @mymarket.name = params[:market][:name]
+    @mymarket.address = params[:market][:address]
+    @mymarket.city = params[:market][:city]
+    @mymarket.county = params[:market][:county]
+    @mymarket.state = params[:market][:state]
+    @mymarket.zip = params[:market][:zip]
+    @mymarket.save
+    redirect_to markets_index_path  
   end
 
   def show
@@ -23,4 +33,5 @@ class MarketsController < ApplicationController
 
   def destroy
   end
+
 end
