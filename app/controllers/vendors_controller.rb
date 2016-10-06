f0474385c923da5a92580e2e1a89e7869bcd18b0
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-10-05 20:13:58 -0700

    Added vendor list of products

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -11,16 +11,10 @@ class VendorsController < ApplicationController
   end
 
   def show
-      @total_sales = []
-      @myvendor = Vendor.find(params[:id].to_i)
-      @mysale = Sale.all
-    @mysale.each do |sale|
-      if @myvendor.id == sale.vendor_id
-        @total_sales << sale.amount
-      @total = @total_sales.inject (0) { |result, element| result + element }
-      end
-    end
-    return @total
+    sales_list
+      total
+      @myvendors = Vendor.find(params[:id])
+      @myproducts=@myvendors.products
   end
 
   def edit
@@ -29,4 +23,43 @@ class VendorsController < ApplicationController
 
   def destroy
   end
-end
+
+  private
+  def total
+    @total_sales = []
+    @myvendor = Vendor.find(params[:id].to_i)
+    @mysale = Sale.all
+    @mysale.each do |sale|
+    if @myvendor.id == sale.vendor_id
+      @total_sales << sale.amount
+    @total = @total_sales.inject (0) { |result, element| result + element }
+    end
+    end
+    return @total
+  end
+
+  def sales_list
+    @total_sales = []
+    @myvendor = Vendor.find(params[:id].to_i)
+    @mysale = Sale.all
+    @mysale.each do |sale|
+    if @myvendor.id == sale.vendor_id
+      @total_sales << sale.amount
+    end
+    end
+    return @total_sales
+  end
+
+
+  def ven_products
+    @list_prods = []
+    @myproducts = Product.find(params[:id].to_i)
+    @myproducts.each do |product|
+      if product.vendor_id == @myvendor.id
+        @list_prods <<  product.name
+      end
+    end
+    return @list_prods
+  end
+
+end#end of class
